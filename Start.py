6e22ab2685fe9d43d8abce507ea8d1912f79b64f
robert.anderson <robert.anderson@vivintsolar.com>
2019-06-13 13:15:23 -0600

    Initial commit

---
+++ Start.py
@@ -0,0 +1,48 @@
+import openpyxl as xl
+from os import path
+from os import walk
+import re
+
+issues = re.compile(r'Batch \d* Issues')
+batch_file = re.compile(r'Batch \d*?')
+batch_number = re.compile(r'\d*')
+
+# VESA Mountable
+
+file = 'C:\\users\\robert.anderson\\Downloads\\Batches\\Batch 277 Funding  Request Details.xlsx'
+
+# wb = xl.load_workbook(file)
+# for i in range(len(wb.get_sheet_names())):
+#     if issues.search(wb.get_sheet_names()[i]):
+#         print('The issues page is found on index ' + str(i) + ' of the workbook' + path.split(file)[1])
+#         print('The name of the sheet is ' + wb.get_sheet_names()[i])
+
+issues_wb = xl.load_workbook(path.join(path.split(file)[0], 'issues.xlsx'))
+issues_page = issues_wb.active
+print(issues_wb.sheetnames)
+
+for folder_name, sub_folders, file_names in walk(path.split(file)[0]):
+    print('The current folder is ' + folder_name)
+    for sub_folder in sub_folders:
+        print('SUBFOLDER OF ' + folder_name + ': ' + sub_folder)
+    for file_name in file_names:
+        print('FILE INSIDE ' + folder_name + ': ' + file_name)
+        if batch_file.search(file_name):
+            print('Opening batch file...')
+            wb = xl.load_workbook(path.join(path.split(file)[0], file_name))
+            print('Searching for issue page...')
+            for i in range(len(wb.sheetnames)):
+                if issues.search(wb.sheetnames[i]):
+                    print('The issues page is found on sheet index ' + str(i) + ' of the current file')
+                    print('The name of the sheet is ' + wb.sheetnames[i])
+                    print('Beginning copy...')
+                    ld_issues = wb[wb.sheetnames[i]]
+                    for row in ld_issues.iter_rows(min_row=2, max_row=ld_issues.max_row, max_col=ld_issues.max_column,
+                                                   values_only=True):
+                        issues_page.append(row)
+                    print('Copy complete')
+
+for row in issues_page.values:
+    print(row)
+
+issues_wb.save(path.join(path.split(file)[0], 'Issues Iterated.xlsx'))
